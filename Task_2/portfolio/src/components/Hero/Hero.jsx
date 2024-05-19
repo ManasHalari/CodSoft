6e2ffd901b232c79be85551739b498ff31436490
ManasHalari <manashalari@gmail.com>
2024-05-18 19:15:15 -1100

    Changed hero section content

--- Task_2/portfolio/src/components/Hero/Hero.jsx
+++ Task_2/portfolio/src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Halari Manas</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with and MERN Stack Developer.I want to make things which is usefull to people.
+          I'm a full-stack Developer with speciallisation in MERN stack.I make things which is usefull to people.Interested in problem-solving and enhancing skill.
         </p>
         <a href="#contact" className={styles.contactBtn}>
           Contact Me
